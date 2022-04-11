a8375aa1b9751cb5970e23c5c866df09871e6d88
hariyono1995 <onnelikrio@gmail.com>
2022-04-11 17:19:52 +0700

    add header

--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -1,11 +1,13 @@
 import styled from "styled-components";
 
+import MenuIcon from "@mui/icons-material/Menu";
+
 export const Container = styled.div`
   position: fixed;
   min-height: 44px;
-  background-color: yellow;
+  /* background-color: yellow; */
   display: flex;
-  /* justify-content: center; */
+  /* justify-content: space-around; */
   align-items: center;
   padding: 0 20px;
   top: 0;
@@ -14,13 +16,36 @@ export const Container = styled.div`
 `;
 
 export const Menu = styled.div`
-  background-color: green;
+  /* background-color: green; */
   text-transform: uppercase;
   display: flex;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
 
-  a {
-    text-decoration: none;
+  p {
     font-weight: 600;
-    font-size: 1rem;
+    font-size: 14px;
+    display: flex;
+    flex-wrap: nowrap;
+    justify-content: center;
+    align-items: center;
+    padding: 0 20px;
+    cursor: pointer;
   }
+`;
+
+export const RightMenu = styled.div`
+  display: flex;
+  align-items: center;
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    margin-right: 10px;
+    font-size: 14px;
+  }
+`;
+
+export const CustomMenu = styled(MenuIcon)`
+  cursor: pointer;
 `;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Container, Menu } from "./Header.styles";
+import { Container, Menu, RightMenu, CustomMenu } from "./Header.styles";
 
 export default function Header() {
   return (
@@ -9,11 +9,24 @@ export default function Header() {
         <img src="/images/logo.svg" alt="Logo Tesla" />
       </a>
       <Menu>
-        <a hreff="#">model s</a>
-        <a hreff="#">model y</a>
-        <a hreff="#">model 3</a>
-        <a hreff="#">model x</a>
+        <p>
+          <a href="#">model s</a>
+        </p>
+        <p>
+          <a href="#">model y</a>
+        </p>
+        <p>
+          <a href="#">model 3</a>
+        </p>
+        <p>
+          <a href="#">model x</a>
+        </p>
       </Menu>
+      <RightMenu>
+        <a href="#">shop</a>
+        <a href="#">tesla account</a>
+        <CustomMenu />
+      </RightMenu>
     </Container>
   );
 }
